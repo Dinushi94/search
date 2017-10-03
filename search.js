47513f2d7ef06e0e605708a9e137abc85974d34a
sigdelsanjog <sigdelsanjog@gmail.com>
2017-10-03 14:39:08 +0545

    regCheck does a regular expression pattern matching for global pattern that contains is in input field

--- search.js
+++ search.js
@@ -1,5 +1,21 @@
+//###########################
+// redirect matches the entered filename and returns the contents of that
+// file only if the pattern exactly matches. For default enter without any filename within the search area
+// it returns all contents inside the documentlocation path provided
+//###########################
 function redirect() {
+    
     // As of now Replace D:/Sanjog/Projects/search with your custom path.
-    document.location.href = 'D:/Sanjog/Projects/search/' + document.getElementById('search').value;
+    var keyword= document.getElementById('search').value;
+    document.location.href = 'D:/Sanjog/Projects/search/' + keyword; 
     return false;
 }
+
+function regCheck(){
+    var pattern = /is/g;
+    var reg = document.getElementById('reg').value;
+    var result= reg.match(pattern);
+    document.getElementById("result").innerHTML= result;
+
+}
+
